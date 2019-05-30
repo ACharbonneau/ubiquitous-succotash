d63fddca5d06a43f53608e0e3249becfe3395939
Amanda Charbonneau <charbo24@msu.edu>
2019-05-30 12:49:17 -0400

    Create 0_GetData.sh

---
+++ 0_GetData.sh
@@ -0,0 +1,15 @@
+# Human gene list with exons, as gff3 
+
+wget -r ftp://ftp.ensembl.org/pub/release-96/gff3/homo_sapiens/
+mv ftp.ensembl.org/ HumanGenes
+
+# Human genome
+
+wget -r ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/supporting/GRCh38_positions/
+
+# Chromatin states Core 15-state model (5 marks, 127 epigenomes)
+
+wget E001_15_coreMarks_hg38lift_dense.bed.gz
+
+
+
