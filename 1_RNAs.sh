048ebd9e434219cd3fe5029f8088e8e2743f6168
Amanda Charbonneau <charbo24@msu.edu>
2019-06-12 15:13:46 -0400

    Update 1_RNAs.sh

--- 1_RNAs.sh
+++ 1_RNAs.sh
@@ -1,6 +1,6 @@
 # We want to find SNPs that fall inside coding regions of antisense, lincRNA, snoRNA, and miRNAs
 
-# First, make a list of chromosome names to go through. It will be faster to loop through them then search every name in every chromosome file
+# First, make a list of features to search for, then loop through searching for them
 
 featurelist="lincRNA antisense snoRNA miRNA"
 
@@ -12,29 +12,3 @@ for feature in `echo ${featurelist}`
    zgrep -f rnas.txt Homo_sapiens.GRCh38.96.chr.gff3.gz | grep 'exon' > "${feature}".txt
    rm rnas.txt 
    done
-   
-# Run the same code, but to get exons for other RNAs
-
-# antisense
-
-for c in `echo $chromo`
-   do
-   zgrep 'antisense' Homo_sapiens.GRCh38.96.chromosome.${c}.gff3.gz | cut -f 2 -d ':' | cut -f 1 -d ';' > rnas.txt   
-   zgrep -f rnas.txt Homo_sapiens.GRCh38.96.chromosome.${c}.gff3.gz | grep 'exon'
-   done >> antisenseExons.txt
-
-# snoRNAs
-
-for c in `echo $chromo`
-   do
-   zgrep 'snorna' Homo_sapiens.GRCh38.96.chromosome.${c}.gff3.gz | cut -f 2 -d ':' | cut -f 1 -d ';' > rnas.txt   
-   zgrep -f rnas.txt Homo_sapiens.GRCh38.96.chromosome.${c}.gff3.gz | grep 'exon'
-   done >> snoExons.txt
-
-# antisense
-
-for c in `echo $chromo`
-   do
-   zgrep 'mirna' Homo_sapiens.GRCh38.96.chromosome.${c}.gff3.gz | cut -f 2 -d ':' | cut -f 1 -d ';' > rnas.txt   
-   zgrep -f rnas.txt Homo_sapiens.GRCh38.96.chromosome.${c}.gff3.gz | grep 'exon'
-   done >> miExons.txt
