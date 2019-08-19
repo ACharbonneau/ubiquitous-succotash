05da06bc5adae9989bf0d5464fb751e3a37ee817
Amanda Charbonneau <charbo24@msu.edu>
2019-08-19 10:12:24 -0400

    Update ConservationByBase.sh

--- ConservationByBase.sh
+++ ConservationByBase.sh
@@ -8,5 +8,5 @@ for line in open("hg38PGCMasterSnps.bed"):
     cols = line.strip().split()
     vals = bw.values(cols[0], int(cols[1]), int(cols[2]))
     new_cons.write("%s, %s, %s, %s, %s, %s, %s\n" % (cols[0], cols[1], cols[2], cols[3], cols[4], cols[5], str(vals[0])))
-    
-bw.close()
+
+exit()
