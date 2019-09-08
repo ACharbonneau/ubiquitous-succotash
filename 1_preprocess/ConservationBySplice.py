da903f29d592092f42ac6bc274a6dc73691d412f
Amanda Charbonneau <charbo24@msu.edu>
2019-09-08 19:21:04 -0400

    Update ConservationBySplice.py

--- 1_preprocess/ConservationBySplice.py
+++ 1_preprocess/ConservationBySplice.py
@@ -2,20 +2,20 @@
 # called from introns.sh
 
 import pyBigWig
-bw = pyBigWig.open("RawData/hg38.phyloP30way.bw")
-new_cons = open("ConsBySplice/ConsBySpliceFirst50.csv",'w')
+bw = pyBigWig.open("../RawData/hg38.phyloP30way.bw")
+new_cons = open("ConsBySpliceFirst50.csv",'w')
 
-for line in open("ConsBySplice/first50_mrna_introns_SNP_Filtered.bed"):
+for line in open("first50_mrna_introns_SNP_Filtered.bed"):
     cols = line.strip().split()
     vals = bw.values(cols[0], int(cols[1]), int(cols[2]))
     new_cons.write("%s, %s, %s, %s, %s, %s, %s\n" % (cols[0], cols[1], cols[2], cols[3], cols[4], cols[5], str(vals[0])))
     
 bw.close()
 
-bw = pyBigWig.open("RawData/hg38.phyloP30way.bw")
-new_cons = open("ConsBySplice/ConsBySpliceLast50.csv",'w')
+bw = pyBigWig.open("../RawData/hg38.phyloP30way.bw")
+new_cons = open("ConsBySpliceLast50.csv",'w')
 
-for line in open("ConsBySplice/last50_mrna_introns_SNP_Filtered.bed"):
+for line in open("last50_mrna_introns_SNP_Filtered.bed"):
     cols = line.strip().split()
     vals = bw.values(cols[0], int(cols[1]), int(cols[2]))
     new_cons.write("%s, %s, %s, %s, %s, %s, %s\n" % (cols[0], cols[1], cols[2], cols[3], cols[4], cols[5], str(vals[0])))
