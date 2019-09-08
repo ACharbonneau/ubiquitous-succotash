d165620d7cfe8b7c9e2ce0d5d9c660515536eb5c
Amanda Charbonneau <charbo24@msu.edu>
2019-09-08 17:12:16 -0400

    Update introns.sh

--- 1_preprocess/introns.sh
+++ 1_preprocess/introns.sh
@@ -1,3 +1,19 @@
+#!/bin/bash --login
+########## SBATCH Lines for Resource Request ##########
+ 
+#SBATCH --time=02:00:00             # limit of wall clock time - how long the job will run (same as -t)
+#SBATCH --nodes=1-5                 # number of different nodes - could be an exact number or a range of nodes (same as -N)
+#SBATCH --ntasks=1                  # number of tasks - how many tasks (nodes) that you require (same as -n)
+#SBATCH --cpus-per-task=2           # number of CPUs (or cores) per task (same as -c)
+#SBATCH --mem-per-cpu=2G            # memory required per allocated CPU (or core) - amount of memory (in bytes)
+#SBATCH --job-name Introns      # you can give your job a name for easier identification (same as -J)
+ 
+########## Command Lines to Run ##########
+ 
+module purge
+
+#module load GCC/6.4.0-2.28 OpenMPI  ### load necessary modules, e.g.
+cd $SLURM_SUBMIT_DIR || exit 
 
 mkdir introns
 
