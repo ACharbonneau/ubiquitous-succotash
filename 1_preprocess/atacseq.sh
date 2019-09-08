54045d834ca75f63ad8fb9635973b9344b9422d4
Amanda Charbonneau <charbo24@msu.edu>
2019-09-08 19:25:16 -0400

    Update atacseq.sh

--- 1_preprocess/atacseq.sh
+++ 1_preprocess/atacseq.sh
@@ -1,4 +1,25 @@
-## Only use ACC_neuron and ACC_glia
+#!/bin/bash --login
+########## SBATCH Lines for Resource Request ##########
+ 
+#SBATCH --time=02:00:00             # limit of wall clock time - how long the job will run (same as -t)
+#SBATCH --nodes=1-5                 # number of different nodes - could be an exact number or a range of nodes (same as -N)
+#SBATCH --ntasks=1                  # number of tasks - how many tasks (nodes) that you require (same as -n)
+#SBATCH --cpus-per-task=2           # number of CPUs (or cores) per task (same as -c)
+#SBATCH --mem-per-cpu=5G            # memory required per allocated CPU (or core) - amount of memory (in bytes)
+#SBATCH --job-name GetRawSNPs      # you can give your job a name for easier identification (same as -J)
+ 
+########## Command Lines to Run ##########
+ 
+module purge
+
+#module load GCC/6.4.0-2.28 OpenMPI  ### load necessary modules, e.g.
+cd $SLURM_SUBMIT_DIR || exit                 ### change to the directory where your code is located
+
+#### NOTES ####
+# Everything in the final analysis is using HG37/HG19
+# But some of the SNP catagories we're using ONLY exist for Hg38, so we are downloading both, 
+# and doing a backwards liftover of some of the files after filtering
+################# Only use ACC_neuron and ACC_glia
 
 mkdir atacseq
 
@@ -13,7 +34,7 @@ featurelist="ACC_neuron ACC_glia"
 # Use bedtools to find the location intersection between the SNP list from NCBI that has SNP locations, atacseq files
 
 for feature in ${featurelist}
-   do bedtools intersect -wa -wb -a ../RawData/hg38PGCMasterSnps.bed -b ../RawData/${feature}.bed > ${feature}_SNP_Locations.txt
+   do bedtools intersect -wa -wb -a ../RawData/NoMHC_GPHN_SNP.bed -b ../RawData/${feature}.bed > ${feature}_SNP_Locations.txt
 done
 
 # Get rid of X, Y chromosomes
@@ -35,3 +56,5 @@ done
 for feature in ${featurelist}
    do awk '{ print $4 }' ${feature}_SNP_Final.txt | sort | uniq  > ${feature}_LSDR.txt
 done
+
+scontrol show job $SLURM_JOB_ID 
