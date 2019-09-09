d8e837c6199ef4961277bfce65ed3fc625724ca5
Amanda Charbonneau <charbo24@msu.edu>
2019-09-08 22:23:27 -0400

    Update introns.sh

--- 1_preprocess/introns.sh
+++ 1_preprocess/introns.sh
@@ -110,7 +110,7 @@ done
 # Make files for LDSR
 
 for feature in ${featurelist}
-   do awk '{ print $4 }' ${feature}_SNP_Filtered.txt | sort | uniq  > ${feature}_LSDR.txt
+   do awk '{ print $4 }' ${feature}_SNP_Filtered.txt | sort | uniq  > ${feature}_LDSR.txt
 done
 
 
@@ -134,7 +134,7 @@ sed 's/, /\t/g' ConsByBase.csv | awk '{if($7>.5)print $1 "\t" $2 "\t" $3 "\t" $4
 #to add faster than expected evolution
 #sed 's/, /\t/g' ConsByBase.csv | awk '{if($7<-.5)print $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6 "\t" $7}' >> ConsByBase.tsv
 
-awk '{ print $4 }' ConsByBase.tsv > ConsByBase_LSDR.txt
+awk '{ print $4 }' ConsByBase.tsv > ConsByBase_LDSR.txt
 
 
 scontrol show job $SLURM_JOB_ID 
