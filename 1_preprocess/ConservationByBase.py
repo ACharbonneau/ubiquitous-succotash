7fd7f1e8ffd8a1618dd6b333675229c453966883
Amanda Charbonneau <charbo24@msu.edu>
2019-09-10 11:45:38 -0400

    Update ConservationByBase.py

--- 1_preprocess/ConservationByBase.py
+++ 1_preprocess/ConservationByBase.py
@@ -7,6 +7,6 @@ new_cons = open("ConsByBase.csv",'w')
 for line in open("../RawData/NoMHC_GPHN_SNP_Hg38.bed"):
     cols = line.strip().split()
     vals = bw.values(cols[0], int(cols[1]), int(cols[2]))
-    new_cons.write("%s, %s, %s, %s, %s, %s, %s\n" % (cols[0], cols[1], cols[2], cols[3], cols[4], cols[5], str(vals[0])))
+    new_cons.write("%s, %s, %s, %s, %s\n" % (cols[0], cols[1], cols[2], cols[3], str(vals[0])))
     
 exit()
