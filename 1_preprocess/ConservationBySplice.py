5bc597645f48a789536528109c200c2fc6ae440a
Amanda Charbonneau <charbo24@msu.edu>
2019-09-10 11:39:37 -0400

    Update ConservationBySplice.py

--- 1_preprocess/ConservationBySplice.py
+++ 1_preprocess/ConservationBySplice.py
@@ -10,8 +10,6 @@ for line in open("first50_mrna_introns_SNP_Filtered_Hg38.bed"):
     vals = bw.values(cols[0], int(cols[1]), int(cols[2]))
     new_cons.write("%s, %s, %s, %s, %s\n" % (cols[0], cols[1], cols[2], cols[3], str(vals[0]))
     
-bw.close()
-
 bw = pyBigWig.open("../RawData/hg38.phyloP30way.bw")
 new_cons = open("ConsBySpliceLast50.csv",'w')
 
@@ -20,4 +18,3 @@ for line in open("last50_mrna_introns_SNP_Filtered_Hg38.bed"):
     vals = bw.values(cols[0], int(cols[1]), int(cols[2]))
     new_cons.write("%s, %s, %s, %s, %s\n" % (cols[0], cols[1], cols[2], cols[3], str(vals[0]))
     
-bw.close()
