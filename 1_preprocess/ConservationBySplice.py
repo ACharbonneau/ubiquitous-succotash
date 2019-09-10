520a7965143058eaef4db1230e9c6a6eae2a6bd3
Amanda Charbonneau <charbo24@msu.edu>
2019-09-10 11:25:20 -0400

    Update ConservationBySplice.py

--- 1_preprocess/ConservationBySplice.py
+++ 1_preprocess/ConservationBySplice.py
@@ -8,7 +8,7 @@ new_cons = open("ConsBySpliceFirst50.csv",'w')
 for line in open("first50_mrna_introns_SNP_Filtered_Hg38.bed"):
     cols = line.strip().split()
     vals = bw.values(cols[0], int(cols[1]), int(cols[2]))
-    new_cons.write("%s, %s, %s, %s, %s, %s, %s\n" % (cols[0], cols[1], cols[2], cols[3], cols[4], cols[5], str(vals[0])))
+    new_cons.write("%s, %s, %s, %s, %s\n" % (cols[0], cols[1], cols[2], cols[3], str(vals[0]))
     
 bw.close()
 
@@ -18,6 +18,6 @@ new_cons = open("ConsBySpliceLast50.csv",'w')
 for line in open("last50_mrna_introns_SNP_Filtered_Hg38.bed"):
     cols = line.strip().split()
     vals = bw.values(cols[0], int(cols[1]), int(cols[2]))
-    new_cons.write("%s, %s, %s, %s, %s, %s, %s\n" % (cols[0], cols[1], cols[2], cols[3], cols[4], cols[5], str(vals[0])))
+    new_cons.write("%s, %s, %s, %s, %s\n" % (cols[0], cols[1], cols[2], cols[3], str(vals[0]))
     
 bw.close()
